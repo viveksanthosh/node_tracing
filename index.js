9d1eb11614bfe0e8ab12828a8039f74471956419
Vivek <viveksanthosh@holidayiq.com>
2020-11-04 08:17:29 +0530

    delay test

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
-
 const server = express()
+const { delay } = require('bluebird')
 
-server.get('/delay', (req, res) => {
-    console.log(process.pid);
+server.get('/delay', async (req, res) => {
     res.status(200).send()
+    const { delayTime, id } = req.query;
+    await doSomething(delayTime, id)
+    console.log(process.pid);
 })
 
 server.listen(4000, () => {
     console.log(process.pid);
     console.log('started');
 })
+
+async function doSomething(delayTime, id) {
+    await delay(delayTime)
+    console.log('done ', id);
+}
