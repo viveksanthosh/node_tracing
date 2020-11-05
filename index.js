1fb6ed40e048531e3ead5ba27ae0e84af5d2ed1c
Vivek <viveksanthosh@holidayiq.com>
2020-11-05 14:27:13 +0530

    Revert "added interperter middleware"
    This reverts commit 2e008d17d0a0fc0caf307fe7e75af1f5c6f421e6.

--- index.js
+++ index.js
@@ -2,21 +2,8 @@ const express = require('express');
 const server = express()
 const { delay } = require('bluebird')
 
-server.use('/', (req, res, next) => {
-    console.log('here');
-    const mapper = { 1: { destination: 1, duration: 2 }, 2: { destination: 2, duration: 2 }, 3: { destination: 1, duration: 2 } }
-    const { id } = req.query
-    if (mapper[id]) {
-        req.query.destination = req.query.destination ? req.query.destination : mapper[id].destination
-        req.query.duration = req.query.duration ? req.query.duration : mapper[id].duration
-    }
-    next()
-
-})
-
 server.get('/delay', async (req, res) => {
     res.status(200).send()
-    console.log(req.query);
     const { delayTime, id } = req.query;
     await doSomething(delayTime, id)
     console.log(process.pid);
