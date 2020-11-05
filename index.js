34522a71c8360443c3a2659aa9d923c73ead0577
Vivek <viveksanthosh@holidayiq.com>
2020-11-05 14:36:52 +0530

    added logging

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const server = express()
+const md5 = require('md5')
 const { delay } = require('bluebird')
 
-server.get('/delay', async (req, res) => {
-    res.status(200).send()
+const timerApi = (req, res, next) => {
+    const time = (new Date()).getTime()
+    let id = md5(Math.random())
+    let enterTime = time
+
+    res.locals.id = id
+    res.locals.enterTime = enterTime
+
+    res.on("finish", () => {
+        const time = (new Date()).getTime()
+        //const { enterTime, id } = res.locals
+        console.log(`time for ${id}->`, time - enterTime);
+    })
+    next()
+}
+
+server.get('/delay', timerApi, async (req, res) => {
     const { delayTime, id } = req.query;
     await doSomething(delayTime, id)
     console.log(process.pid);
+    res.status(200).send()
 })
 
 server.listen(4000, () => {
