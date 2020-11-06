ebeb740d9c4900d79307671f51afc2e73f563776
Vivek <viveksanthosh@holidayiq.com>
2020-11-06 06:45:21 +0530

    refined hook example

--- hook_example.js
+++ hook_example.js
@@ -5,20 +5,23 @@ const nsid = 'a6a29a6f-6747-4b5f-b99f-07ee96e32f88';
 const session = cls.createNamespace(nsid); //namespace
 
 for (let i = 0; i < 5; i++) {
-    Promise.resolve().then(() => {
-        const delayTime = Math.floor(Math.random() * 1000)
-        session.run(async () => {
-            session.set('delay', delayTime)
-            session.set('i', i)
-            console.log(delayTime, '--', i)
-            await processInformation()
+    const delayTime = Math.floor(Math.random() * 10000)
+    session.run(async () => {
+        session.set('delay', delayTime)
+        session.set('i', i)
+        console.log(delayTime, '--', i)
+        processInformation(delayTime).then(() => { })
 
-        })
     })
 }
 
-function processInformation(delayTime){
+function processInformation(delayTime) {
     return delay(delayTime).then(() => {
-        console.log(session.get('delay'), '----', session.get('i'))
+        console.log('here')
+        console.log(session.get('delay'), '--++--', session.get('i'))
     })
 }
+
+delay(10000).then(() => {
+    console.log('done');
+})
